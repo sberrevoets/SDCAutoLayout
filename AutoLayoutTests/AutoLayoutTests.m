ebd149a0e9cf0795f5f841f3775415a494eadf2e
Scott Berrevoets <s.berrevoets@me.com>
2014-09-24 21:14:03 -0700

    Improve code readability

--- AutoLayoutTests/AutoLayoutTests.m
+++ AutoLayoutTests/AutoLayoutTests.m
@@ -16,22 +16,16 @@
 
 @implementation AutoLayoutTests
 
-- (void)setUp
-{
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
-
-- (void)  testAssertFail {
+- (void)testTopLevelViewThrowsWhenCenteredInSuperView {
 	UIView *aView = [[UIView alloc] initWithFrame:CGRectZero];
-	[aView sdc_centerInSuperview];
+	XCTAssertThrows([aView sdc_centerInSuperview], @"");
 }
 
-- (void) testAssertPass {
+- (void)testSubviewDoesNotThrowWhenCenteredInSuperView {
 	UIView *aView = [[UIView alloc] initWithFrame:CGRectZero];
 	UIView *aParentView = [[UIView alloc] initWithFrame:CGRectZero];
 	[aParentView addSubview:aView];
-	[aView sdc_centerInSuperview];
+	XCTAssertNoThrow([aView sdc_centerInSuperview], @"");
 }
 
 - (void)testCommonAncestorWithView {
@@ -57,17 +51,6 @@
 	XCTAssertEqualObjects(parent, [parent sdc_commonAncestorWithView:subchild1], @"Common ancestor should be parent");
 	XCTAssertEqualObjects(subchild1, [subchild1child1 sdc_commonAncestorWithView:subchild1child2], @"Common ancestor should be subchild1");
 	XCTAssertNil([dummyView sdc_commonAncestorWithView:subchild1], @"dummyView and subchild1 do not have a common ancestor");
-}
-
-- (void)tearDown
-{
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
-
-- (void)testExample
-{
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
 }
 
 @end
